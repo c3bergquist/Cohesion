30b1b52186538378071eb29b86d88257d4b631b4
hkuns <hkuns91@gmail.com>
2015-02-10 22:42:46 -0500

    Player Controller, Main Scene Changes
    Made the char controller, made a script to destroy 'drops' on collision

---
+++ Cohesion/Assets/Scripts/CharController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		
+		
+	}
+}
