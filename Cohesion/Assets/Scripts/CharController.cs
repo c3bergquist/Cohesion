0d0456369eba64c0d2de314a44414f82af586245
hkuns <hck27@n1-140-189.dhcp.drexel.edu>
2015-02-17 13:05:55 -0500

    texture changing

--- Cohesion/Assets/Scripts/CharController.cs
+++ Cohesion/Assets/Scripts/CharController.cs
@@ -6,6 +6,8 @@ public class CharController : MonoBehaviour {
 	public float growMultiplier = 1.01f;
 	public float maxSpeed = 10f;
 
+	public Material[] materials;
+
 	private bool reachedBottom = false;
 	
 	void OnCollisionEnter (Collision col) {
@@ -32,6 +34,22 @@ public class CharController : MonoBehaviour {
 		rigidbody.velocity = new Vector2 (move * maxSpeed, rigidbody.velocity.y);
 
 		rigidbody.drag = 2 * Mathf.Sin(Time.time) + 7;
+
+		if (DropletManager.Instance.dropsCollected < 5) {
+			GameObject.FindGameObjectWithTag ("Body").renderer.material = materials[0];
+		}
+		else if (DropletManager.Instance.dropsCollected <=10 && DropletManager.Instance.dropsCollected > 5) {
+			GameObject.FindGameObjectWithTag ("Body").renderer.material = materials[1];
+		}
+		else if (DropletManager.Instance.dropsCollected <=15 && DropletManager.Instance.dropsCollected > 10) {
+			GameObject.FindGameObjectWithTag ("Body").renderer.material = materials[2];
+		}
+		else if (DropletManager.Instance.dropsCollected <=20 && DropletManager.Instance.dropsCollected > 15) {
+			GameObject.FindGameObjectWithTag ("Body").renderer.material = materials[3];
+		}
+		else if (DropletManager.Instance.dropsCollected <=25 && DropletManager.Instance.dropsCollected > 20) {
+			GameObject.FindGameObjectWithTag ("Body").renderer.material = materials[4];
+		}
 	}
 
 	public void Grow () {
